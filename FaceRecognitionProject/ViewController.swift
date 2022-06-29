8a0c10d4b7b8614be16fd9291ec65d952f636abc
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-29 12:51:14 +0300

    .

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func scanButtonClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) { // error object awaits pointer type "error" so put "&"
+ 
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it You?") { success, error in
+                
+                if success == true {
+                    //successfull auth
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toDetailVC", sender: nil)
 
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.errorLabel.text = "Error"
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
 }
 
